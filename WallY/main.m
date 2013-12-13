1c7f5f00e7905379ba45866cb51bf18b6f79ca5c
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-12-13 01:36:02 +0100

    Initial Commit

---
+++ WallY/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WallY
+//
+//  Created by Johan Lindskogen on 2013-12-13.
+//  Copyright (c) 2013 Johan Lindskogen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
